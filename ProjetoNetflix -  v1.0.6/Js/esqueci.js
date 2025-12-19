f9ca5f84708f9aefe8b9ede22895558204b18a6f
Pedro Caldeira <pedrocordeiro.caldeira27@gmail.com>
2025-12-18 21:31:58 -0300

    esqueci feito

--- ProjetoNetflix -  v1.0.6/Js/esqueci.js
+++ ProjetoNetflix -  v1.0.6/Js/esqueci.js
@@ -1,59 +1,43 @@
-// Busca os usuários cadastrados no LocalStorage
-const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+const formulario = document.getElementById("formTrocarSenha");
+const inputSenha = document.getElementById("senha");
+const inputConfirmar = document.getElementById("confirmar");
 
-// Seleciona elementos da página
-const formulario = document.querySelector("form");
-const inputEmail = document.querySelector("input[type='text']");
-const inputSenha = document.querySelector("input[type='password']");
-const botaoEntrar = document.querySelector("form .submit");
-
-// Ação de login
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const senha = inputsenha.value.trim();
+  const senha = inputSenha.value.trim();
   const confirmar = inputConfirmar.value.trim();
 
-  // Validação básica
   if (senha === "" || confirmar === "") {
     alert("Preencha todos os campos.");
     return;
   }
 
-  // Verifica se o usuário existe
-  const usuarioEncontrado = usuarios.find(
-    (u) => u.senha.toLowerCase() === senha.toLowerCase() && u.confirmar === confirmar
-  );
-
-  if (!usuarioEncontrado) {
-    alert("As senhas nao se concidem!");
+  if (senha !== confirmar) {
+    alert("As senhas não coincidem!");
     return;
   }
 
-  // Armazena usuário logado (opcional)
-  localStorage.setItem("usuarioLogado", JSON.stringify(usuarioEncontrado));
-
-  // Redireciona
-  alert("Senha trocada com sucesso!");
-  window.location.href = "MainPage.html"; // ajuste o nome se for diferente
+  alert("Senha confirmada com sucesso!");
+  window.location.href = "MainPage.html";
 });
 
-const botaoCadastro = document.getElementById("btnCadastro");
-
-botaoCadastro.addEventListener("click", () => {
-  window.location.href = "cadastro.html"; // coloque o nome correto do arquivo
-});
 function showHidePassword() {
-  const password = document.getElementById('confirmar');
+  const password = document.getElementById('senha');
   const toggler = document.querySelector('.input i');
 
   if (password.type === 'password') {
     password.type = 'text'; //  mostra a senha
-    toggler.classList.remove('bi-eye-slash');
-    toggler.classList.add('bi-eye-fill');
+    //toggler.classList.remove('bi-eye-slash');
+    //toggler.classList.add('bi-eye-fill');
   } else {
     password.type = 'password'; //  esconde a senha
-    toggler.classList.remove('bi-eye-fill');
-    toggler.classList.add('bi-eye-slash');   
+   // toggler.classList.remove('bi-eye-fill');
+    //toggler.classList.add('bi-eye-slash');   
 }
 }
+
+
+
+
+
