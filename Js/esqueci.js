a3ef0af1329801d84152674da29cb48bdae77ad0
Pedro Caldeira <pedrocordeiro.caldeira27@gmail.com>
2025-12-23 19:21:08 -0300

    favicons

--- Js/esqueci.js
+++ Js/esqueci.js
@@ -6,13 +6,44 @@ const msgErro = document.getElementById("msgErro");
 
 // Validação da senha
 function validarSenha(senha) {
- if (senha.length < 8) return "A senha deve ter pelo menos 8 caracteres.";
+  if (senha.length < 8) return "A senha deve ter pelo menos 8 caracteres.";
   if (!/[0-9]/.test(senha)) return "A senha deve ter pelo menos um número.";
-  if (!/[A-Z]/.test(senha)) return "A senha deve ter pelo menos uma letra maiúscula.";
-  if (!/[@#$%&]/.test(senha)) return "A senha deve ter pelo menos um símbolo (@#$%&).";
-  return "";''
+  if (!/[A-Z]/.test(senha))
+    return "A senha deve ter pelo menos uma letra maiúscula.";
+  if (!/[@#$%&]/.test(senha))
+    return "A senha deve ter pelo menos um símbolo (@#$%&).";
+
+  return ""; // Sem erro
 }
 
+function validarSenhaAoDigitar() {
+  const senha = campoSenha.value;
+
+  const regra8 = document.getElementById("regra8");
+  const regraNumero = document.getElementById("regraNumero");
+  const regraMaiuscula = document.getElementById("regraMaiuscula");
+  const regraSimbolo = document.getElementById("regraSimbolo");
+
+  // sempre esconde tudo antes
+  regra8.style.display = "none";
+  regraNumero.style.display = "none";
+  regraMaiuscula.style.display = "none";
+  regraSimbolo.style.display = "none";
+
+  // agora mostra só a primeira regra que falhar
+  if (senha.length < 8) {
+    regra8.style.display = "block";
+  } 
+  else if (!/[0-9]/.test(senha)) {
+    regraNumero.style.display = "block";
+  } 
+  else if (!/[A-Z]/.test(senha)) {
+    regraMaiuscula.style.display = "block";
+  } 
+  else if (!/[@#$%&]/.test(senha)) {
+    regraSimbolo.style.display = "block";
+  }
+}
 // Checar senhas e mostrar mensagem
 function checarSenhas() {
   if (campoSenha.value === "" || campoConfirmar.value === "") {
